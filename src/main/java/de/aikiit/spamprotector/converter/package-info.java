f57e155a116490997daf0f6f7262742e7ab01c4d
Hugo Hirsch <github@hugo-hirsch.de>
2015-12-05 22:13:15 +0100

    Issue #10: Fix javadoc errors.

---
+++ src/main/java/de/aikiit/spamprotector/converter/package-info.java
@@ -0,0 +1,19 @@
+/** Base package for converter logics of the SpamSchutz/SpamProtector application. */
+package de.aikiit.spamprotector.converter;
+/**
+ SpamSchutz - simple way to protect your mail adresses from naiive spammers
+ Copyright (C) 2011, Aiki IT
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
