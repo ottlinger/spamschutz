04677084721ef9d9d306268ad6a7328be31d07c1
Hugo Hirsch <github@hugo-hirsch.de>
2025-05-18 12:58:51 +0200

    Run mutationtests during site build

--- site.sh
+++ site.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Generating new mvn site ..."
-./mvnw clean site:site 
+./mvnw clean org.pitest:pitest-maven:mutationCoverage site:site 
 echo "DONE - ready to commit and push"
 git checkout gh-pages && ./copy-site.sh 
