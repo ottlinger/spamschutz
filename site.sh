b9b1dbd99f5dc71d17d9db29c629d7a28e75b30f
Hugo Hirsch <github@hugo-hirsch.de>
2025-05-18 13:08:42 +0200

    Generate pitest report requires different goal before

--- site.sh
+++ site.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Generating new mvn site ..."
-./mvnw clean org.pitest:pitest-maven:mutationCoverage site:site 
+./mvnw clean test-compile org.pitest:pitest-maven:mutationCoverage site:site 
 echo "DONE - ready to commit and push"
 git checkout gh-pages && ./copy-site.sh 
